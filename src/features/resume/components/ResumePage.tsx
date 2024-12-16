39458e35747167c26039dbcf1cb5d4c90f6542c6
Charles Russell <chjrussell@gmail.com>
2024-12-15 17:53:30 -0700

    Fix ReactPDFViewer

--- src/features/resume/components/ResumePage.tsx
+++ src/features/resume/components/ResumePage.tsx
@@ -1,23 +1,7 @@
 "use client";
-import dynamic from "next/dynamic";
-import Resume from "./reactPDF/Resume";
 
-const PDFViewer = dynamic(
-  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex h-full w-full items-center justify-center">
-        <div className="flex items-end gap-2">
-          <span className="text-4xl font-bold">Loading Resume</span>
-          <div className="mb-1.5 h-2 w-2 animate-bounce rounded-full bg-foreground [animation-delay:-0.3s]"></div>
-          <div className="mb-1.5 h-2 w-2 animate-bounce rounded-full bg-foreground [animation-delay:-0.13s]"></div>
-          <div className="mb-1.5 h-2 w-2 animate-bounce rounded-full bg-foreground"></div>
-        </div>
-      </div>
-    ),
-  },
-);
+import { PDFViewer } from "./reactPDF/PDFView";
+import Resume from "./reactPDF/Resume";
 
 export default function ResumePage() {
   return (
---
+++ src/features/resume/components/reactPDF/ReactPDFViewerExporter.tsx
@@ -0,0 +1,5 @@
+import { PDFViewer } from "@react-pdf/renderer";
+
+const ReactPDFViewer = PDFViewer;
+
+export default ReactPDFViewer;
