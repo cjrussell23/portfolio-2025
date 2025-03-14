6e23106f6c52314547fc4246386e07fc04375d70
Charles Russell <chjrussell@gmail.com>
2025-03-13 22:36:08 -0600

    Add revalidation for Home and Resume pages to improve data freshness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import HomePage from "@/features/home/components/HomePage";
 
+export const revalidate = 60 * 10;
+
 export default async function Home() {
   return <HomePage />;
 }
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,7 @@
 import ResumePage from "@/features/resume/components/ResumePage";
 
+export const revalidate = 60 * 10;
+
 export default function Page() {
   return <ResumePage />;
 }
