af437405a9a41174333b184eeffe40e278173b3f
Charles Russell <chjrussell@gmail.com>
2025-03-13 22:43:28 -0600

    Update revalidation duration for Home and Resume pages to improve data freshness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HomePage from "@/features/home/components/HomePage";
 
-export const revalidate = 60 * 10;
+export const revalidate = 600;
 
 export default async function Home() {
   return <HomePage />;
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,6 @@
 import ResumePage from "@/features/resume/components/ResumePage";
 
-export const revalidate = 60 * 10;
+export const revalidate = 600;
 
 export default function Page() {
   return <ResumePage />;
