247feb4c28e99b49ac1705118af6c4787294c690
Charles Russell <chjrussell@gmail.com>
2025-03-18 21:41:35 -0600

    Add @vercel/analytics for enhanced performance tracking in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { ViewTransitions } from "next-view-transitions";
@@ -22,6 +23,7 @@ export default function RootLayout({
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </ViewTransitions>
